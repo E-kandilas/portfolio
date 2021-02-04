f72d7d40b902a1ccb4968e31ef36f078969b8364
Emmanuel Kandilas <ekandilas@gmail.com>
2021-02-04 15:55:32 -0500

    fixed profile picture css to behave as intended

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -4,12 +4,14 @@ import aboutText from "./Assets/Text/aboutText";
 const AboutMe = () => {
   return (
     <div className="container-xl">
-      <div className="row justify-content-around">
-        <div className="col-md">
-          <img className="profile-picture" src={headShot3} />
+      <div className="row justify-content-center">
+        <div className="col-md ">
+          <div className="profile-image">
+            <img className="" src={headShot3} />
+          </div>
         </div>
 
-        <div className="col-xl">
+        <div className="col-xl justify-content-center">
           <h3>Emmanuel Kandilas</h3>
           <p className="text-justify">{aboutText}</p>
         </div>
