90547d8215c31a195c15490a9f00cb8f45387429
Emmanuel Kandilas <ekandilas@gmail.com>
2021-02-04 19:47:52 -0500

    changed icons to svg files

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,9 +1,15 @@
 import headShot3 from "./Assets/Images/headShot3.jpg";
 import aboutText from "./Assets/Text/aboutText";
 
+//Images Import
+import GitHub from "./Assets/Images/Icons/github-brands.svg";
+import Linkedin from "./Assets/Images/Icons/linkedin-brands.svg";
+import Email from "./Assets/Images/Icons/envelope-solid.svg";
+import Resume from "./Assets/Images/Icons/file-alt-solid.svg";
+
 const AboutMe = () => {
   return (
-    <div className="container-xl">
+    <div className="content">
       <div className="row justify-content-center">
         <div className="col-md ">
           <div className="profile-image">
@@ -11,9 +17,26 @@ const AboutMe = () => {
           </div>
         </div>
 
-        <div className="col-xl justify-content-center">
-          <h3>Emmanuel Kandilas</h3>
-          <p className="text-justify">{aboutText}</p>
+        <div className="col-xl">
+          <h3>Welcome</h3>
+          <p className="text-justify text-body">{aboutText}</p>
+          <div className="homepage-links">
+            <a href="https://github.com/E-kandilas" target="_blank">
+              <img src={GitHub} className="home-icon" alt="Github" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/manny-kan-code/"
+              target="_blank"
+            >
+              <img src={Linkedin} className="home-icon" alt="LinkedIn" />
+            </a>
+            <a href="" target="_blank">
+              <img src={Email} className="home-icon" alt="Email" />
+            </a>
+            <a href="" target="_blank">
+              <img src={Resume} className="home-icon" alt="Resume" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- src/App.js
+++ src/App.js
@@ -5,10 +5,8 @@ import Projects from "./Projects";
 const App = () => {
   return (
     <div className="App">
-      <div className="col">
         <AboutMe />
         <Projects />
-      </div>
     </div>
   );
 };
