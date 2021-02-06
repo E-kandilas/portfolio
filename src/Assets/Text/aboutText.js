570ba02143feab38bfcecde3a3c79721291e1f67
Emmanuel Kandilas <ekandilas@gmail.com>
2021-02-06 18:52:19 -0500

    removed unused pictures and changed picture image location to reflect changes

--- src/Assets/Text/aboutText.js
+++ src/Assets/Text/aboutText.js
@@ -1,6 +1,6 @@
 const aboutText = `My name is Emmanuel. I am a software engineer on a never-ending quest to better
 my abilities. I get a kick out of simple, scalable code and minimal design. You can download my 
-resume or check out my other media and projects below if you wish to learn more about me. 
+resume or check out my other media and projects below if you wish to learn more. 
 I am open to all opportunities and always happy to chat.
 `;
 
--- src/Projects.js
+++ src/Projects.js
@@ -1,13 +1,12 @@
 import ProjectCard from "./ProjectCard";
-import mindOverMatterDash from "./Assets/Images/MindOverMatter/DashBoard.png";
-import battleBoggleLogo from "./Assets/Images/BattleBoggle/BattleBoggle.png";
-import glife1 from "./Assets/Images/GestureLife/GLifeHome.png"
-import glife2 from "./Assets/Images/GestureLife/GLifeSkus.png"
+import mindOverMatterDash from "./Assets/Images/Projects/DashBoard.png";
+import battleBoggleLogo from "./Assets/Images/Projects/BattleBoggle.png";
+import glife1 from "./Assets/Images/Projects/GLifeHome.png"
 
 
 const Projects = () => {
   return (
-    <div className="card-deck pt-5 pl-2 ">
+    <div className="card-deck pt-5 pl-2 w-75">
       <ProjectCard picture={glife1} title={"Gesture Life"} />
       <ProjectCard picture={mindOverMatterDash} title={"Mind Over Matter"} />
       <ProjectCard picture={battleBoggleLogo} title={"Battle Boggle"}/>
