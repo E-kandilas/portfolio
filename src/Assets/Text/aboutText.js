7be4197bb9f093ee3a7331100b17c5f4a2fb0abe
Emmanuel Kandilas <ekandilas@gmail.com>
2021-02-06 16:21:37 -0500

    removed keyframe shake on mouseover because it does not fit with the minimal design

--- src/Assets/Text/aboutText.js
+++ src/Assets/Text/aboutText.js
@@ -1,5 +1,5 @@
 const aboutText = `My name is Emmanuel. I am a software engineer on a never ending quest to better
-my abilities. I get a kick out of simple, scalable code as well as minimal design. You can download my 
+my abilities. I get a kick out of simple, scalable code as and minimal design. You can download my 
 resume or check out my other media and projects below if you wish to learn more about me. 
 I am open to all opportunities and always happy to chat.
 `;
