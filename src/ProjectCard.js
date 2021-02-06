c4ae176546b9e6a228e488c8007e49b1471c6b4a
Emmanuel Kandilas <ekandilas@gmail.com>
2021-02-06 01:17:43 -0500

    basic carousel behavior for projects cards

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -8,38 +8,21 @@ const ProjectCard = (props) => {
       <div className="card">
         <div
           id="carouselExampleIndicators2"
-          class="carousel slide"
+          className="carousel slide"
           data-ride="carousel"
         >
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img class="w-100" src={picture1} alt="image" />
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img className="w-100" src={picture1} alt="image" />
             </div>
-            <div class="carousel-item">
-              <img class="w-100" src={picture2} alt="image" />
+            <div className="carousel-item">
+              <img className="w-100" src={picture2} alt="image" />
             </div>
-            <div class="carousel-item">
-              <img class="w-100" src={picture3} alt="image" />
+            <div className="carousel-item">
+              <img className="w-100" src={picture3} alt="image" />
             </div>
           </div>
-          <a
-            class="carousel-control-prev"
-            href="#carouselExampleIndicators2"
-            role="button"
-            data-slide="prev"
-          >
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a
-            class="carousel-control-next"
-            href="#carouselExampleIndicators2"
-            role="button"
-            data-slide="next"
-          >
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
+
           <div className="card-body">
             <h5 className="card-title">Mind Over Matter</h5>
             <p className="card-text">Sphynx of black quartz judge my vow</p>
