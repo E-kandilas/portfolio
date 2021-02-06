9de79801d8cca4a559fdaf6bf5efb5b59245a997
Emmanuel Kandilas <ekandilas@gmail.com>
2021-02-06 16:09:12 -0500

    added media query to background image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import AboutMe from "./AboutMe";
 import Projects from "./Projects";
+import blossom from "./Assets/Images/ink-plum-blossom.png"
 
 const App = () => {
   return (
-    <div className=".container-md h-100 pt-5">
+    <div className=".container-fluid pt-5">
+      <img className="background-img" src={blossom}/>
         <AboutMe />
         <Projects />
     </div>
